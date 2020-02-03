96df5eeb94bc5ab2628bcc41ed0046e96ddd1593
ju <wowow1859@naver.com>
2020-02-03 22:24:34 +0900

    Board Project first commit!

---
+++ board-api/src/main/java/kr/co/wancs/board/domain/Board.java
@@ -0,0 +1,10 @@
+package kr.co.wancs.board.domain;
+
+public class Board {
+
+  private int boardNo;
+  private String title;
+  private String content;
+  private int views;
+
+}
---
+++ board-api/src/test/java/kr/co/wancs/board/domain/BoardTests.java
@@ -0,0 +1,17 @@
+package kr.co.wancs.board.domain;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.BDDMockito.given;
+
+import org.junit.jupiter.api.Test;
+
+public class BoardTests {
+
+  @Test
+  public void creation() {
+    // when
+    Board board = new Board();
+
+    // then
+  }
+}
